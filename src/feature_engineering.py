04f9c72cf4455a0d117b4696f7764857e8c7c60c
marianataglio <marianataglio@gmail.com>
2023-07-26 20:29:39 -0300

    Refactor#3: * import arg parse * Complete docstring * Refactor input path, use os for robustness * create run method * add main method to parse arguments. * call main object in main statement.

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -9,6 +9,8 @@ FECHA: 20/07/2023
 
 # Imports
 import pandas as pd
+import os
+import argparse
 
 
 class FeatureEngineeringPipeline(object):
@@ -19,21 +21,20 @@ class FeatureEngineeringPipeline(object):
 
     def read_data(self) -> pd.DataFrame:
         """
-        COMPLETAR DOCSTRING 
-
+        COMPLETAR DOCSTRING  
+        Reads train and test data, concatenates both sets to perform feature engineering. 
         :return pandas_df: The desired DataLake table as a DataFrame
         :rtype: pd.DataFrame
         """
 
-        data_train = pd.read_csv('../data/Train_BigMart.csv')
-        #data_test = pd.read_csv('../data/Test_BigMart.csv')
+        data_train = pd.read_csv(os.path.join(self.input_path, 'Train_BigMart.csv'))
+        data_test = pd.read_csv(os.path.join(self.input_path,'Test_BigMart.csv'))
         # Identificando la data de train y de test, para posteriormente unión y separación
-        '''data_train['Set'] = 'train'
+        data_train['Set'] = 'train'
         data_test['Set'] = 'test'
-        data = pd.concat([data_train, data_test], ignore_index=True, sort=False)'''
+        data = pd.concat([data_train, data_test], ignore_index=True, sort=False)
 
-        return data_train 
-        #data_test
+        return data
 
     
     def data_transformation(self, df: pd.DataFrame) -> pd.DataFrame:
@@ -45,11 +46,10 @@ class FeatureEngineeringPipeline(object):
         :rtype: pd.Dataframe
     
         """
-        # AÑOS DEL ESTABLECIMIENTO
+        #AÑOS DEL ESTABLECIMIENTO
         df['Outlet_Establishment_Year'] = 2020 - df['Outlet_Establishment_Year']
-        #Unificando etiquetas para Item_fat__content
 
-       # FEATURES ENGINEERING: creando categorías para 'Item_Type'
+        #FEATURES ENGINEERING: creando categorías para 'Item_Type'
         df['Item_Type'] = df['Item_Type'].replace({'Others': 'Non perishable', 'Health and Hygiene': 'Non perishable', 'Household': 'Non perishable',
         'Seafood': 'Meats', 'Meat': 'Meats',
         'Baking Goods': 'Processed Foods', 'Frozen Foods': 'Processed Foods', 'Canned': 'Processed Foods', 'Snack Foods': 'Processed Foods',
@@ -66,7 +66,7 @@ class FeatureEngineeringPipeline(object):
         df.loc[df['Item_Type'] == 'Soft Drinks', 'Item_Fat_Content'] = 'NA'
         df.loc[df['Item_Type'] == 'Fruits and Vegetables', 'Item_Fat_Content'] = 'NA'
 
-         # FEATURES ENGINEERING: asignación de nueva categorías para 'Item_Fat_Content'
+        #FEATURES ENGINEERING: asignación de nueva categorías para 'Item_Fat_Content'
         df.loc[df['Item_Type'] == 'Non perishable', 'Item_Fat_Content'] = 'NA'
 
         #IMPUTACION POR MODA DE FALTANTES DE ITEM_WEIGHT
@@ -75,7 +75,7 @@ class FeatureEngineeringPipeline(object):
             moda = (df[df['Item_Identifier'] == producto][['Item_Weight']]).mode().iloc[0,0]
             df.loc[df['Item_Identifier'] == producto, 'Item_Weight'] = moda
 
-        #limpieza de faltantes en el tamaño de las tiendas
+        #LIMPIEZA DE FALTANTES EN EL TAMAÑO DE LAS TIENDAS
         outlets = list(df[df['Outlet_Size'].isnull()]['Outlet_Identifier'].unique())
         for outlet in outlets:
             df.loc[df['Outlet_Identifier'] == outlet, 'Outlet_Size'] =  'Small'
@@ -84,13 +84,9 @@ class FeatureEngineeringPipeline(object):
         print(pd.qcut(df['Item_MRP'], 4,).unique())
         df['Item_MRP'] = pd.qcut(df['Item_MRP'], 4, labels = [1, 2, 3, 4])
 
-        #Se utiliza una copia de data para separar los valores codificados en un dataframe distinto.
-        #dataframe = df.drop(columns=['Item_Type', 'Item_Fat_Content']).copy()
-
-        # Codificación de variables ordinales
+        #CODIFICACIÓN DE VARIABLES ORDINALES
         df['Outlet_Size'] = df['Outlet_Size'].replace({'High': 2, 'Medium': 1, 'Small': 0})
         df['Outlet_Location_Type'] = df['Outlet_Location_Type'].replace({'Tier 1': 2, 'Tier 2': 1, 'Tier 3': 0}) # Estas categorias se ordenaron asumiendo la categoria 2 como más lejos
-
         df = pd.get_dummies(df, columns=['Outlet_Type'], drop_first=True)
 
         #PREPARANDO DATA DE ENTRENAMIENTO Y TEST
@@ -101,8 +97,9 @@ class FeatureEngineeringPipeline(object):
 
     def write_prepared_data(self, transformed_dataframe: pd.DataFrame) -> None:
         """
-        COMPLETAR DOCSTRING
-        
+        Write tranformed dataframe to the output_path
+        :param transformed_dataframe: the tranformed dataframe to be written
+        :type transformed_dataframe: pd.Dataframe
         """
  
         transformed_dataframe.to_csv(self.output_path, index=False)
@@ -114,9 +111,19 @@ class FeatureEngineeringPipeline(object):
     
         df = self.read_data()
         df_transformed = self.data_transformation(df)
-        self.write_prepared_data(df_transformed)
+        self.write_prepared_data_train(df_transformed)
 
-  
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-path")
+    parser.add_argument("--output-path")
+
+    args = parser.parse_args()
+    FeatureEngineeringPipeline(input_path = args.input_path,
+                               output_path = args.output_path).run()
+
+
 if __name__ == "__main__":
-    FeatureEngineeringPipeline(input_path = 'Ruta/De/Donde/Voy/A/Leer/Mis/Datos',
-                               output_path = 'Ruta/Donde/Voy/A/Escribir/Mi/Archivo').run()
+    main()
+    
