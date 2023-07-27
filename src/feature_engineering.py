476314feeb64022f8fac3ddb0c00b0eefe9a9011
marianataglio <marianataglio@gmail.com>
2023-07-27 00:01:59 -0300

    Refactor4: eliminate transformations of unused features, remove print, drop columns Item_Fat_Content and Item-Type, in write_prepared_data, split trasnformed df into train and test df based on the set column and write train and test df to csv

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -49,26 +49,6 @@ class FeatureEngineeringPipeline(object):
         #AÑOS DEL ESTABLECIMIENTO
         df['Outlet_Establishment_Year'] = 2020 - df['Outlet_Establishment_Year']
 
-        #FEATURES ENGINEERING: creando categorías para 'Item_Type'
-        df['Item_Type'] = df['Item_Type'].replace({'Others': 'Non perishable', 'Health and Hygiene': 'Non perishable', 'Household': 'Non perishable',
-        'Seafood': 'Meats', 'Meat': 'Meats',
-        'Baking Goods': 'Processed Foods', 'Frozen Foods': 'Processed Foods', 'Canned': 'Processed Foods', 'Snack Foods': 'Processed Foods',
-        'Breads': 'Starchy Foods', 'Breakfast': 'Starchy Foods',
-        'Soft Drinks': 'Drinks', 'Hard Drinks': 'Drinks', 'Dairy': 'Drinks'})
-       
-        #NORMALIZACIÓN ITEM_FAT_CONTENT
-        df['Item_Fat_Content'] = df['Item_Fat_Content'].replace({'low fat':  'Low Fat', 'LF': 'Low Fat', 'reg': 'Regular'})
-
-        #FEATURES ENGINEERING: asignación de nueva categorías para 'Item_Fat_Content'
-        df.loc[df['Item_Type'] == 'Household', 'Item_Fat_Content'] = 'NA'
-        df.loc[df['Item_Type'] == 'Health and Hygiene', 'Item_Fat_Content'] = 'NA'
-        df.loc[df['Item_Type'] == 'Hard Drinks', 'Item_Fat_Content'] = 'NA'
-        df.loc[df['Item_Type'] == 'Soft Drinks', 'Item_Fat_Content'] = 'NA'
-        df.loc[df['Item_Type'] == 'Fruits and Vegetables', 'Item_Fat_Content'] = 'NA'
-
-        #FEATURES ENGINEERING: asignación de nueva categorías para 'Item_Fat_Content'
-        df.loc[df['Item_Type'] == 'Non perishable', 'Item_Fat_Content'] = 'NA'
-
         #IMPUTACION POR MODA DE FALTANTES DE ITEM_WEIGHT
         productos = list(df[df['Item_Weight'].isnull()]['Item_Identifier'].unique())
         for producto in productos:
@@ -81,19 +61,22 @@ class FeatureEngineeringPipeline(object):
             df.loc[df['Outlet_Identifier'] == outlet, 'Outlet_Size'] =  'Small'
 
         #SEPARACION DE PRECIO EN CUANTILES
-        print(pd.qcut(df['Item_MRP'], 4,).unique())
         df['Item_MRP'] = pd.qcut(df['Item_MRP'], 4, labels = [1, 2, 3, 4])
 
         #CODIFICACIÓN DE VARIABLES ORDINALES
         df['Outlet_Size'] = df['Outlet_Size'].replace({'High': 2, 'Medium': 1, 'Small': 0})
         df['Outlet_Location_Type'] = df['Outlet_Location_Type'].replace({'Tier 1': 2, 'Tier 2': 1, 'Tier 3': 0}) # Estas categorias se ordenaron asumiendo la categoria 2 como más lejos
-        df = pd.get_dummies(df, columns=['Outlet_Type'], drop_first=True)
+        
+        #ONE HOT ENCODING DE OUTLET TYPE
+        df = pd.get_dummies(df, columns=['Outlet_Type'])
 
         #PREPARANDO DATA DE ENTRENAMIENTO Y TEST
         # Eliminación de variables que no contribuyen a la predicción por ser muy específicas
-        df.drop(columns=['Item_Identifier', 'Outlet_Identifier'])
+        df.drop(columns=['Item_Fat_Content', 'Item_Type', 'Item_Identifier', 'Outlet_Identifier'])
 
-        return df
+        df_transformed = df.copy()
+
+        return df_transformed
 
     def write_prepared_data(self, transformed_dataframe: pd.DataFrame) -> None:
         """
@@ -101,9 +84,21 @@ class FeatureEngineeringPipeline(object):
         :param transformed_dataframe: the tranformed dataframe to be written
         :type transformed_dataframe: pd.Dataframe
         """
- 
-        transformed_dataframe.to_csv(self.output_path, index=False)
-        #df_test.to_csv("test_final.csv")
+        #SPLIT TRANSFORMED DF INTO TRAIN AND TEST DF BASED ON THE SET COLUMN
+        df_train = transformed_dataframe[transformed_dataframe['Set'] == 'train']
+        df_test = transformed_dataframe[transformed_dataframe['Set'] == 'test']
+
+        df_train.drop(['Set'], axis=1, inplace=True)
+        df_test.drop(['Set'], axis=1, inplace=True)
+
+        train_output_path = os.path.join(self.output_path, 'train_final.csv')
+        test_output_path = os.path.join(self.output_path, 'test_final.csv')
+
+        #WRITE TRAIN DF TO CSV
+        df_train.to_csv(train_output_path, sep=',', index=False)
+
+        #WRITE TEST DF TO CSV
+        df_test.to_csv(test_output_path, sep=',', index=False)  
         
         return None
 
@@ -111,8 +106,9 @@ class FeatureEngineeringPipeline(object):
     
         df = self.read_data()
         df_transformed = self.data_transformation(df)
-        self.write_prepared_data_train(df_transformed)
-
+        self.write_prepared_data(df_transformed)
+        
+        
 
 def main():
     parser = argparse.ArgumentParser()
