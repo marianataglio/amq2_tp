801f77e08e25ff0f5748c284d4120104c9b4f136
marianataglio <marianataglio@gmail.com>
2023-08-07 18:32:05 -0300

    Refactor #6: properly working train pipeline

--- src/train.py
+++ src/train.py
@@ -1,14 +1,23 @@
 """
 train.py
 
-COMPLETAR DOCSTRING
-
-DESCRIPCIÓN:
-AUTOR:
-FECHA:
+DESCRIPCIÓN: TRAINING DE MODELO DE ML CON ESCRITURA EN FORMATO .PKL
+AUTOR:MARIANA TAGLIO
+FECHA: 07/08/2022
 """
 
 # Imports
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+from sklearn.model_selection import train_test_split, cross_validate, cross_val_score
+from sklearn import metrics
+from sklearn.linear_model import LinearRegression
+import pickle
+import argparse
+import os
+
 
 class ModelTrainingPipeline(object):
 
@@ -18,36 +27,77 @@ class ModelTrainingPipeline(object):
 
     def read_data(self) -> pd.DataFrame:
         """
-        COMPLETAR DOCSTRING 
-        
-        :return pandas_df: The desired DataLake table as a DataFrame
+        Read the training data from a csv file and returns a pandas DataFrame.        
+        :return pandas_df: DataFrame containing the training data. 
         :rtype: pd.DataFrame
         """
-            
-        # COMPLETAR CON CÓDIGO
-        
-        return pandas_df
+        df_train = pd.read_csv(os.path.join(self.input_path, 'train_final.csv'))
+
+        return df_train
 
     
     def model_training(self, df: pd.DataFrame) -> pd.DataFrame:
         """
-        COMPLETAR DOCSTRING
-        
-        """
-        
-        # COMPLETAR CON CÓDIGO
-        
-        return df_transformed
+        Train a linear regression model using the provided DataFrame.
 
+        :param df: The DataFrame containing the training data.
+        :type df: pd.DataFrame
+        :return: The DataFrame with training data (unchanged).
+        :rtype: pd.DataFrame        
+        """
+        seed = 28
+        model = LinearRegression()
+
+        # Split into training and validation sets
+        X = df.drop(columns='Item_Outlet_Sales') #[['Item_Weight', 'Item_MRP', 'Outlet_Establishment_Year', 'Outlet_Size', 'Outlet_Location_Type']] # .drop(columns='Item_Outlet_Sales')
+        x_train, x_val, y_train, y_val = train_test_split(X, df['Item_Outlet_Sales'], test_size = 0.3, random_state=seed)
+
+        # Train model
+        model.fit(x_train,y_train)
+
+        # Prediction over validation set
+        pred = model.predict(x_val)
+
+        # Calculate mean square error and rr (coefficient of determination)
+        mse_train = metrics.mean_squared_error(y_train, model.predict(x_train))
+        R2_train = model.score(x_train, y_train)
+        print('Métricas del Modelo:')
+        print('ENTRENAMIENTO: RMSE: {:.2f} - R2: {:.4f}'.format(mse_train**0.5, R2_train))
+
+        mse_val = metrics.mean_squared_error(y_val, pred)
+        R2_val = model.score(x_val, y_val)
+        print('VALIDACIÓN: RMSE: {:.2f} - R2: {:.4f}'.format(mse_val**0.5, R2_val))
+
+        print('\nCoeficientes del Modelo:')
+        # Print model constant
+        print('Intersección: {:.2f}'.format(model.intercept_))
+
+        # Model coefficients
+        coef = pd.DataFrame(x_train.columns, columns=['features'])
+        coef['Coeficiente Estimados'] = model.coef_
+        print(coef, '\n')
+        
+        return df
+
+  
     def model_dump(self, model_trained) -> None:
         """
-        COMPLETAR DOCSTRING
+        Save a trained machine learning model in pkl format.
         
+        :param model_trained: The trained machine learning model to be saved.
+        :type model_trained: object
+        :return: None
         """
-        
-        # COMPLETAR CON CÓDIGO
-        
+   
+        #Save the model as model.pkl in the specified directory
+        model_file_path = os.path.join(os.path.dirname(self.model_path), 'model.pkl')
+        with open(model_file_path, 'wb') as f:
+            pickle.dump(model_trained, f)
+
+        print(f"Model saved successfully in '{self.model_path}', as 'model.pkl'.")
+
         return None
+          
 
     def run(self):
     
@@ -55,7 +105,17 @@ class ModelTrainingPipeline(object):
         model_trained = self.model_training(df)
         self.model_dump(model_trained)
 
-if __name__ == "__main__":
 
-    ModelTrainingPipeline(input_path = 'Ruta/De/Donde/Voy/A/Leer/Mis/Datos',
-                          model_path = 'Ruta/Donde/Voy/A/Escribir/Mi/Modelo').run()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_path", type=str, help="Path to the input data CSV file.")
+    parser.add_argument("--model_path", type=str, help="Path to save the trained model in pkl format.")
+
+    args = parser.parse_args()
+    ModelTrainingPipeline(input_path = args.input_path,
+                            model_path = args.model_path).run()
+
+
+if __name__ == "__main__":
+    main()
+
