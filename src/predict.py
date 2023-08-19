eaeb0dc1e8336f5407422a28a24880bfbf1ee94c
marianataglio <marianataglio@gmail.com>
2023-08-19 03:34:33 -0300

    Refactor: load data method, load model that imports a pkl file which is a Pipeline, make_predictions: predicts using the step model from the pipeline, write_predictions to csv file

--- src/predict.py
+++ src/predict.py
@@ -21,10 +21,10 @@ class MakePredictionPipeline(object):
         Class implementing models prediction. Receives as `input_path` transformed data output of feature_engineering.py,
         laods model from `model_path`, and writes predictions on `output_path`.
         """
-        def __init__(self, input_path, output_path, model_path):
+        def __init__(self, input_path, output_path, models_path):
             self.input_path = input_path
             self.output_path = output_path
-            self.model_path = model_path
+            self.models_path = models_path
                     
 
         def load_data(self) -> pd.DataFrame:
@@ -33,57 +33,61 @@ class MakePredictionPipeline(object):
             :return pred_data: DataFrame containing the test data. 
             :rtype: pd.DataFrame
             """
-            pred_data = pd.read_csv(os.path.join(self.input_path))
-
-            return pred_data
-
+            data_to_pred = pd.read_csv(os.path.join(self.input_path))
+            return data_to_pred
 
 
         def load_model(self) -> None:
             """
             Loads best model. Imports pickle file.
             """    
-
+             # Print the model path for debugging
+            print("Model path:", self.models_path)
             #ToDo: change to PRODUCTION_PATH eventually
 
-            with open(self.model_path, 'rb') as model_file:
-                self.model = pickle.load(model_file)           
+            with open(os.path.join(self.models_path, 'model.pkl'), 'rb') as f:
+                self.model = pickle.load(f)           
+            print("Pipeline loaded:", self.model) 
 
 
-       # def make_predictions(self, data: DataFrame) -> pd.DataFrame:
+        def make_predictions(self, data: pd.DataFrame) -> pd.DataFrame:
             """
             COMPLETAR DOCSTRING
             """
-    
-       #     new_data = self.model.predict(data)
+            #Apply transformations 
+            model_pipeline = self.model
+            predicted_data = model_pipeline.named_steps['model'].predict(data)
+            return predicted_data
 
-        #   return new_data
+           # predicted_data = self.model.predict(data)
+           # return predicted_data
+       
 
-        #def write_predictions(self, predicted_data: DataFrame) -> None:
+        def write_predictions(self, predicted_data: pd.DataFrame) -> None:
             """
             COMPLETAR DOCSTRING
-            """
-
+            """ 
+            output_df = pd.DataFrame({'Predicted_Item_Outlet_Sales': predicted_data})
+            output_df.to_csv(self.output_path, index=False)
          #   return None 
 
         def run(self):
 
             data = self.load_data()
             self.load_model()
-          #  df_preds = self.make_predictions(data)
-          #  self.write_predictions(df_preds)
-
+            df_preds = self.make_predictions(data)
+            self.write_predictions(df_preds)
 
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input-path", type=str, help="Path to the input data CSV file.")
-    parser.add_argument("--models-path", type=str, help="Path to save the trained model in pkl format.")
+    parser.add_argument("--models-path", type=str, help="Path to load the trained model in pkl format.")
+    parser.add_argument("--output-path", type=str, help="Path to save predictions")
 
     args = parser.parse_args()
-    MakePredictionPipeline(input_path = args.input_path,
-                            models_path = args.models_path).run()
+    MakePredictionPipeline(input_path = args.input_path, models_path = args.models_path, output_path = args.output_path).run()
 
 
 if __name__ == "__main__":
