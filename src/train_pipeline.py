2189a00fb550a75074d02a6adebd9c8cb7f97b04
marianataglio <marianataglio@gmail.com>
2023-08-21 19:07:17 -0300

    Fix: tidy. Output path is now PRODUCTION_PATH

--- src/train_pipeline.py
+++ src/train_pipeline.py
@@ -6,5 +6,11 @@ from config import ROOT_PATH, load_config
 
 config = load_config(os.path.join(ROOT_PATH, "config.yaml"))
 
-subprocess.run('Python feature_engineering.py --input-path {DATA_PATH} --output-path {TFMD_DATA_PATH}'.format(**config).split())
-subprocess.run('Python train.py --input-path {TFMD_DATA_PATH} --models-path {MODELS_PATH}'.format(**config).split())
+subprocess.run(
+    'Python feature_engineering.py --input-path {DATA_PATH} --output-path {TFMD_DATA_PATH}'
+    .format(**config).split()
+    )
+subprocess.run(
+    'Python train.py --input-path {TFMD_DATA_PATH} --models-path {PRODUCTION_PATH}'
+    .format(**config).split()
+)
