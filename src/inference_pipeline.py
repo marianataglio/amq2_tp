8179914c276a1590c6b101158e5db07ce3525046
marianataglio <marianataglio@gmail.com>
2023-08-21 19:07:59 -0300

    Fix

--- src/inference_pipeline.py
+++ src/inference_pipeline.py
@@ -3,10 +3,10 @@ import os
 
 from config import ROOT_PATH, load_config
 
-def main():
+config = load_config(os.path.join(ROOT_PATH, "config.yaml"))
 
-    config = load_config(os.path.join(ROOT_PATH, "config.yaml"))
-    
-    subprocess.run('Python feature_engineering.py --input-path {DATA_PATH} --output-path {TFMD_DATA_PATH}'.format(**config).split())
-    subprocess.run('Python predict.py --input-path {TFMD_DATA_PATH}/test_final.csv --models-path {MODELS_PATH} --output-path {PREDS_PATH}'.format(**config).split())
-
+#subprocess.run('Python feature_engineering.py --input-path {DATA_PATH} --output-path {TFMD_DATA_PATH}'.format(**config).split())
+subprocess.run(
+    'Python predict.py --input-path {TFMD_DATA_PATH} --models-path {PRODUCTION_PATH}' 
+                     ' --output-path {PREDS_PATH}'.format(**config).split()
+)
