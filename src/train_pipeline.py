85b6596c0bf4117dba412a72a0ae688f0e06d66f
marianataglio <marianataglio@gmail.com>
2023-08-21 20:11:54 -0300

    Refactor: no longer using config.yaml

--- src/train_pipeline.py
+++ src/train_pipeline.py
@@ -1,16 +1,26 @@
 import subprocess
-import os 
-
-from config import ROOT_PATH, load_config
+import argparse
 
 
-config = load_config(os.path.join(ROOT_PATH, "config.yaml"))
+parser = argparse.ArgumentParser(description="Train pipeline script")
+parser.add_argument("--input-file", required=True, help="Name of the input file in DATA_PATH")
+parser.add_argument("--tfmd-dir", required=True, help="Path to transformed csv and pkl")
+parser.add_argument("--model-file", required=True, help="Path to model .pkl")
+args = parser.parse_args()
 
-subprocess.run(
-    'Python feature_engineering.py --input-path {DATA_PATH} --output-path {TFMD_DATA_PATH}'
-    .format(**config).split()
-    )
-subprocess.run(
-    'Python train.py --input-path {TFMD_DATA_PATH} --models-path {PRODUCTION_PATH}'
-    .format(**config).split()
+config = dict(input_file = args.input_file,
+                tfmd_dir = args.tfmd_dir,
+                model_file = args.model_file)
+
+fe_command = (
+    'python feature_engineering.py --input-path {input_file} --output-path {tfmd_dir}'
+    .format(**config)
 )
+
+train_command = (
+    'python train.py --input-path {tfmd_dir} --models-path {model_file}'
+    .format(**config)
+)
+
+subprocess.run(fe_command, shell=True)
+subprocess.run(train_command, shell=True)
