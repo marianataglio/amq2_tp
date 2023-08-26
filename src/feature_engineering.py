e3f7bfa659a8af7828ff2c8e804b7327d6182eb1
marianataglio <marianataglio@gmail.com>
2023-08-26 18:24:00 -0300

    Docs: add docstring and fix comments

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -15,10 +15,18 @@ import pickle
 
 
 import pandas as pd
-from transformations import ModeImputation, OutletYearTransformer, NullImputer, OrdinalEncoderTransformer, \
-    OneHotEncoder, PriceBucketsTransformer, RemoveColumns
+from transformations import YearTransformer, NullImputer, OrdinalEncoderTransformer, \
+    OneHotEncoder, PriceBucketsTransformer, RemoveColumns, ModeImputationByGroup
 
 class FeatureEngineeringPipeline(object):
+    """
+        Initializes the FeatureEngineeringPipeline object.
+        
+        :param input_path: Path to the input data CSV file.
+        :type input_path: str
+        :param output_path: Path to the output directory for transformed data CSV file and pkl.
+        :type output_path: str
+        """
 
     def __init__(self, input_path, output_path):
         self.input_path = input_path
@@ -26,9 +34,8 @@ class FeatureEngineeringPipeline(object):
 
     def read_data(self) -> pd.DataFrame:
         """
-        COMPLETAR DOCSTRING  
-        Reads train and test data, concatenates both sets to perform feature engineering. 
-        :return pandas_df: The desired DataLake table as a DataFrame
+        Reads data to perform feature engineering. 
+        :return data: The desired DataLake table as a DataFrame
         :rtype: pd.DataFrame
         """
 
@@ -37,10 +44,15 @@ class FeatureEngineeringPipeline(object):
         return data
 
     def run_pipeline(self):
+        """
+        Runs the feature engineering pipeline
+        """
+
         df = self.read_data()
 
         price_columns = ['Item_MRP']
-        impute_mode_columns = ['Item_Weight']
+        impute_columns = 'Item_Weight'
+        group_column = 'Item_Identifier'
         outlet_year_columns = ['Outlet_Establishment_Year']
         null_imputer_columns = ['Outlet_Size',]
         null_value = 'Small'
@@ -51,35 +63,26 @@ class FeatureEngineeringPipeline(object):
         one_hot_columns = ['Outlet_Type']
         columns_to_drop = ['Item_Fat_Content', 'Item_Type', 'Item_Identifier', 'Outlet_Identifier']
 
-
  
         pipeline = Pipeline([
             ('price_buckets', PriceBucketsTransformer(columns=price_columns, num_bins=4, labels=[1,2,3,4])),
-            ('impute_mode', ModeImputation(columns=impute_mode_columns)),
-            ('outlet_year', OutletYearTransformer(columns=outlet_year_columns)),
+            ('impute_mode', ModeImputationByGroup(impute_column=impute_columns, group_column=group_column)),
+            ('outlet_year', YearTransformer(columns=outlet_year_columns)),
             ('outlet_size_imputer', NullImputer(value= null_value, columns=null_imputer_columns)),
             ('ordinal_encoder', OrdinalEncoderTransformer(column_mappings=ordinal_encoder_mapping)),
             ('one_hot', OneHotEncoder(columns=one_hot_columns)),
             ('remove_columns', RemoveColumns(columns=columns_to_drop))
         ])
-
-        # Set the mappings for the ordinal encoder
-        #ordinal_encoder = pipeline.named_steps['ordinal_encoder']
-        #for column, mapping in ordinal_encoder_mapping.items():
-        #    ordinal_encoder.set_mapping(column, mapping)
-
+    
         # Fit and transform the data using the pipeline
         transformed_data = pipeline.fit_transform(df)
+        print("data transformed")
 
         # Save the transformed data to pkl
-        with open(os.path.join(self.output_path, "data_transformed.pkl"), "wb") as pkl_file:
+        with open(os.path.join(self.output_path, "pipeline.pkl"), "wb") as pkl_file:
             pickle.dump(pipeline, pkl_file)
         
         transformed_df = pd.DataFrame(transformed_data)
-
-        # Drop columns
-        #columns_to_drop = ['Item_Fat_Content', 'Item_Type', 'Item_Identifier', 'Outlet_Identifier']
-        #transformed_df.drop(columns=columns_to_drop, inplace=True)
 
         # Save the transformed data to csv
         transformed_df.to_csv(os.path.join(self.output_path, "train_transformed.csv"), index=False)
